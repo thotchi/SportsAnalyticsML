a9b0ac29ae97eefd5aec5af0b1eb380541f849fe
TH <ispykerwin@gmail.com>
2016-03-01 18:51:31 -0800

    THIS SHIT DON'T WORK ON WINDOW 8.1
    CAN'T QUITE FIGURE OUT WHAT'S GOING ON.  GET A RESPONSE 4000 FROM THE
    SERVER.  WORKS IF I USE THE GENERATED URL IN THE BROWSER.  GOT TO TRY A
    DIFFERENT PLATFORM TO SEE IF THAT'S THE PROBLEM.

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -5,9 +5,11 @@ import math
 import traceback
 
 """
-This script scrapes a subset of games of a given NBA season based on two arguments:
-First argument: The subset of games to scrape defined by NUM_GAMES_A_SEASON (1230) and the "total". Currently, it can range from 1 to 7
-Second argument: The first year of the NBA season without millenium or century (e.g. the 2013-14 season would be "13")
+This script scrapes a subset of games of a given NBA season based on two 
+arguments: First argument: The subset of games to scrape defined by 
+NUM_GAMES_A_SEASON (1230) and the "total". Currently, it can range from 1 to 7
+Second argument: The first year of the NBA season without millenium or century 
+(e.g. the 2013-14 season would be "13")
 """
 
 NUM_GAMES_A_SEASON = 82 * 30 / 2
@@ -15,16 +17,28 @@ SEASONS_RANGE = range(0, 16)
 GAMES_RANGE = range(1, NUM_GAMES_A_SEASON + 1)
 
 def scrape(start, stop, season):
-  DATA_DIR = 'data'
-  if not os.path.exists(DATA_DIR):
-    os.makedirs(DATA_DIR)
+    DATA_DIR = 'data'
+    if not os.path.exists(DATA_DIR):
+        os.makedirs(DATA_DIR)
 
-  def passer(request, e, **kwargs):
-      pass
+    def passer(request, e, **kwargs):
+        pass
 
-  def save_json(response, **kwargs):
-    boxscore_json = response.json()
-    try:
+    def save_json(response, **kwargs):
+        print response; # I GET <Response [400]> WHICH SUGGESTS THAT THE
+                        # THE SERVER RETURNED AN ERROR
+                        #
+                        # 10.4.1 400 Bad Request
+                        # The request could not be understood by the server 
+                        # due to malformed syntax. The client SHOULD NOT 
+                        # repeat the request without modifications.
+
+        boxscore_json = response.json()
+
+        # DOESN'T MAKE IT PAST response.json().  SOMEHOW, THIS RETURNS AFTER
+        # CALLING THIS.
+        
+#        try:
         if boxscore_json.get('Message') == 'An error has occurred.':
             return
         result_sets = boxscore_json['resultSets']
@@ -33,36 +47,58 @@ def scrape(start, stop, season):
         unique_id = "{}-{}.json".format(date, game_id)
         with open(os.path.join(DATA_DIR, unique_id), 'w') as outfile:
             json.dump(boxscore_json, outfile)
-    except Exception as e:
-        traceback.print_exc()
-    finally:
-        response.close()
+#        except Exception as e:
+#            traceback.print_exc()
+#        finally:
+#            response.close()
 
-  async_list = []
-  for game_num in GAMES_RANGE[start:stop]:
-      game_id = "002{:02}0{:04}".format(season, game_num)
-      url = 'http://stats.nba.com/stats/boxscore/?GameId={}&StartPeriod=0&EndPeriod=0&StartRange=0&EndRange=0&RangeType=0'.format(game_id)
-      action_item = grequests.get(url, hooks={'response': save_json})
-      async_list.append(action_item)
-  grequests.map(async_list, size=len(async_list), stream=False, exception_handler=passer)
+    async_list = []
+    for game_num in GAMES_RANGE[start:stop]:
+        game_id = "002{:02}0{:04}".format(season, game_num)
+        url = 'http://stats.nba.com/stats/boxscore/?GameId={}'.format(game_id);
+        url += '&StartPeriod=0&EndPeriod=0&StartRange=0&EndRange=0';
+        url += '&RangeType=0';
+
+        print url;  # URL LOOKS LIKE IT'S OKAY.  I CAN USE THE URL IN THE 
+                    # BROWSER AND CAN DOWNLOAD A JSON FILE.
+
+        action_item = grequests.get(url, hooks={'response': save_json})
+        async_list.append(action_item)
+
+    grequests.map(
+        async_list
+        ,size=len(async_list)
+        ,stream=False
+        ,exception_handler=passer
+    )
 
 if __name__ == "__main__":
-  import argparse
-  parser = argparse.ArgumentParser()
-  parser.add_argument('start', type=int, help='The index in the season to start scraping.')
-  parser.add_argument('stop', type=int, help='The index in the season to stop scraping.')
-  parser.add_argument(
-    'season',
-    type=int,
-    help='The season to scrape (for example: 13 for the 2013-2014 season)',
-    nargs='?',
-  )
-  args = parser.parse_args()
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        'start'
+        ,type=int
+        ,help='The index in the season to start scraping.'
+    )
+    parser.add_argument(
+        'stop'
+        ,type=int
+        ,help='The index in the season to stop scraping.'
+    )
+    parser.add_argument(
+        'season'
+        ,type=int
+        ,help='The season to scrape (for example: 13 for the 2013-2014 season)'
+        ,nargs='?'
+    )
+    args = parser.parse_args()
 
-  try:
-    if not (0 <= int(args.season) <= 16):
-      raise ValueError()
-  except (TypeError, ValueError):
-    raise Exception('Season must be a number between 0 and 15 (for example: 13 for the 2013-2014 season)')
+    try:
+        if not (0 <= int(args.season) <= 16):
+            raise ValueError()
+    except (TypeError, ValueError):
+        raise Exception('Season must be a number between 0 and 15 ' + \
+            '(for example: 13 for the 2013-2014 season)'
+            )
 
-  scrape(args.start, args.stop, args.season)
+    scrape(args.start, args.stop, args.season)
